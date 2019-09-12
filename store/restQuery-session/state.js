25c744b838a9c59cf2edeca10b23fc8ecef79349
Loïc Ronvel <loic.ronvel@gmail.com>
2019-09-12 20:18:00 +0200

    WIP

--- store/restQuery-session/state.js
+++ store/restQuery-session/state.js
@@ -1,21 +1,22 @@
+/*
+The full session Object:
+{
+	userId: null ,
+	token: null ,
+	type: null ,
+	agentId: null ,
+	creationTime: null ,
+	expirationTime: null ,
+	duration: null ,
+}
+localCreationTime: null ,
+localExpirationTime: null ,
+*/
+
+
 export default function() {
 	return {
 		session: JSON.parse( localStorage.getItem( 'session' ) ) ,
-
-		/*
-	{
-		userId: null ,
-		token: null ,
-		type: null ,
-		agentId: null ,
-		creationTime: null ,
-		expirationTime: null ,
-		duration: null ,
-	}
-	localCreationTime: null ,
-	localExpirationTime: null ,
-	*/
-
 		profile: JSON.parse( localStorage.getItem( 'profile' ) ) ,
 		regenerateTimeout: false ,
 
